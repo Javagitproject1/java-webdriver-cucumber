ae669b6b5172a6e9f2b659604ca3bd00da4e7547
Javagitproject1 <macforweb@gmail.com>
2020-08-12 18:13:52 -0400

    Version 1.1

--- src/test/java/definitions/JavaStepDefs.java
+++ src/test/java/definitions/JavaStepDefs.java
@@ -3,6 +3,9 @@ package definitions;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import org.openqa.selenium.remote.server.handler.DeleteSession;
+
+import java.util.Arrays;
 
 public class JavaStepDefs {
 
@@ -10,18 +13,18 @@ public class JavaStepDefs {
     public void iSay(String message) {
 
         String firstName="David";
-        String lastName="Gherkin.";
+        String lastName="Bowie.";
         String color="Green";
         String greeting = "Hi, my name is";
         String greeting1 = "My favorite color is";
         String fulltext= greeting + " "+ firstName + " " + lastName + " " + greeting1 + " " +color;
 
-        System.out.println(fulltext);
-        System.out.println(firstName.getClass());
-        System.out.println(firstName.length()+lastName.length()+color.length());
-        System.out.println(firstName.toUpperCase());
-        System.out.println(firstName.trim());
         System.out.println(message);
+        System.out.println("greeting: " + (fulltext));
+        System.out.println("greeting length:" + (fulltext.length()));
+        System.out.println("upper case:" + (firstName.toUpperCase()));
+        System.out.println("lower case:" + (lastName.toLowerCase()));
+
     }
 
 
@@ -52,19 +55,165 @@ public class JavaStepDefs {
     @Then("I concatenate {string} with {string}")
     public void iConcatenateWith(String str3, String str4) {
 
-        System.out.println("str3: " + str3);
-        System.out.println("str4: " + str4);
+        System.out.println("city1: " + str3);
+        System.out.println("city2: " + str4);
 
-        System.out.println("str3: "+ str3.toUpperCase());
-        System.out.println("str4: "+ str4.toUpperCase());
+        System.out.println("upper case: " + str3.toUpperCase());
+        System.out.println("lower case: " + str4.toLowerCase());
 
-        System.out.println("str3: "+ str3.length());
-        System.out.println("str4: "+ str4.length());
+        System.out.println("city1 length: " + str3.length());
+        System.out.println("city2 length: " + str4.length());
 
-        System.out.println(str3.equals(str4));
-        System.out.print(str3.equalsIgnoreCase(str4));
-        System.out.print(str3.contains(str4));
+        System.out.println("city1 = city2:" + (str3.equals(str4)));
+        System.out.println("city1 contains city2:" + (str3.contains(str4)));
+        System.out.println ("concatenation:" + (str3 + " " + (str4)));
 
     }
 
+    //Values for variables assigned in step definition
+    @Given("I have num1 {int} and num2 {int}")
+    public void iHaveNum1AndNum2(int num1, int num2) {
+
+        System.out.println("num1:"+ (num1));
+        System.out.println("num2:"+ (num2));
+        System.out.println("sum: " + (num1 + (num2)));
+        System.out.println("difference:" + (num1 - (num2)));
+        System.out.println("quotient:" + (num1 % (num2)));
+        System.out.println("product:" + (num1 * (num2)));
+
+        //Values for variables assigned inside the Java code
+
+        //int num1 = 20;
+        //int num2 = 33;
+
+        //System.out.println("num1:" +(num1));
+        //System.out.println("num1:" +(num2));
+        //System.out.println("sum:"+ (num1 + (num2)));
+        //System.out.println("difference:"+ (num1 - (num2)));
+        //System.out.println("quotient:"+ (num1 % (num2)));
+        //System.out.println("product:"+ (num1 * (num2)));
+
+    }
+
+
+    @Given("I like {string} color")
+    public void iLikeColor(String str0) {
+        String notFavoriteColor = "Black";
+
+        System.out.println("Favorite Color:" +(str0));
+        System.out.println("Not Favorite Color:" + (notFavoriteColor));
+        System.out.println("Do colors equal?:" + (str0.equals(notFavoriteColor)));
+    }
+
+    @Given("I print url for {string} page")
+    public void IPrintUrlForPag(String str0) {
+        if (str0.equals("google")){
+            System.out.println("google url is:" + ("https://www.google.com/"));
+        } else if (str0.equals("Google")){
+            System.out.println("Google url is:" + ("https://www.google.com/"));
+        } else {
+            System.out.println ("Sorry, not google:" + ("https://skryabin.com/webdriver/html/sample.html"));
+        }
+
+
+    }
+
+
+    @And("I print if number {int} is positive")
+    public void iPrintIfNumberIsPositive(int int0) {
+
+        if (int0 > 0) {
+            System.out.println("positive");
+        } else {
+            System.out.println("negative");
+        }
+
+    }
+
+    //using If/else for confiditions
+    @And("I print {string} th day of the week")
+    public void iPrintThDayOfTheWeek(String str1) {
+
+        if (str1.equals("1")) {
+            System.out.println("Monday");
+        } else if (str1.equals("2")){
+            System.out.println("Tuesday");
+        } else if (str1.equals("3")){
+            System.out.println("Wednesday");
+        } else  if (str1.equals("4")){
+            System.out.println("Thursday");
+        } else if (str1.equals("5")){
+            System.out.println("Friday");
+        } else if (str1.equals("6")){
+            System.out.println("Saturday");
+        } else if (str1.equals("7")){
+            System.out.println("Sunday");
+        } else{
+            System.out.println("Please enter numbers from 1 to 7");
+        }
+    }
+
+    //Using switch for conditions
+    @And("I print {string} th day of the week using switch")
+    public void iPrintThDayOfTheWeekUsingSwitch(String day0) {
+        switch(day0){
+            case "1":
+                System.out.println("Monday");
+                break;
+            case"2":
+                System.out.println("Tuesday");
+                break;
+            case "3":
+                System.out.println("Wednesday");
+                break;
+            case"4":
+                System.out.println("Thursday");
+                break;
+            case"5":
+                System.out.println("Friday");
+                break;
+            case"6":
+                System.out.println("Saturday");
+                break;
+            case"7":
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Unsupported number. Use numbers from 1 to 7");
+        }
+    }
+
+
+    @Given("I print url for {string} page using break")
+    public void iPrintUrlForPageUsingBreak(String page) {
+
+        switch(page){
+            case "Google":
+                System.out.println("Google url is:" + ("https://www.google.com/"));
+                break;
+            case "google":
+                System.out.println("google url is:" + ("https://www.google.com/"));
+                break;
+            case "bing":
+                System.out.println("bing url is:" + ("https://bing.com") );
+                break;
+            default:
+                System.out.println("Error:" + ("Unsupported page"));
+
+        }
+    }
+
+
+    @Given("I have grocery list")
+    public void iHaveGroceryList() {
+        String [] groceryList = {"apple","banana","orange","watermelon","pineapple"};
+
+        System.out.println();
+        System.out.println(groceryList [0]);
+        System.out.println(groceryList[2]);
+
+    }
+
+
 }
+
--- src/test/resources/features/java.feature
+++ src/test/resources/features/java.feature
@@ -2,18 +2,44 @@
 Feature: My First Java Feature
 
   @java1
-  Scenario: Create Java Steps
-    Given I say "Hello World!"
-    #And You say "All is great!"
-    #And You say "What about you?"
-    #And I say "Can't be better"
+  Scenario: Create Java Step
+    Given I say "Hello! My name is Sasha"
+
 
   @java2
   Scenario: Declare variables
     Given I say firstName "John"
     And I say lastName "Boldwin"
     And I say favoriteColor "Green"
-    Then I concatenate "var1" with "Var1"
+    Then I concatenate "New York" with "San Francisco"
+
+  @java3
+  Scenario: Exercise with numbers
+    Given I have num1 12 and num2 10
+
+  @java4
+  Scenario: Exercise with bolleans (compare colors)
+    Given I like "Red" color
+
+  @java5
+  Scenario: Exercise with Conditions
+    Given I print url for "google" page
+    And I print if number -10 is positive
+    And I print "3" th day of the week
+    And I print "7" th day of the week using switch
+
+    
+  @java6
+  Scenario: Exercise with conditions using break
+    Given I print url for "bing" page using break
+
+  @java7
+  Scenario: Exercise with arrays
+    Given I have grocery list
+
+
+
+
 
 
 
